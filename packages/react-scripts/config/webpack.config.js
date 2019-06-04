d931c944313705660e01cc7082061d9f0e1b0839
Verric <beijing-planes@hotmail.com>
2019-06-04 11:22:29 +1000

    changed webpack config to use myadbox urls

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -7,7 +7,7 @@
  */
 // @remove-on-eject-end
 'use strict';
-
+const productName = process.env.PRODUCT_NAME;
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -184,14 +184,14 @@ module.exports = function(webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
+        ? `${productName}/static/js/[name].[contenthash:8].js`
+        : isEnvDevelopment && `${productName}/static/js/bundle.js`,
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
+        ? `${productName}/static/js/[name].[contenthash:8].chunk.js`
+        : isEnvDevelopment && `${productName}/static/js/[name].chunk.js`,
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
       publicPath: publicPath,
@@ -393,7 +393,7 @@ module.exports = function(webpackEnv) {
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: `${productName}/static/media/[name].[hash:8].[ext]`,
               },
             },
             // Process application JS with Babel.
@@ -567,7 +567,7 @@ module.exports = function(webpackEnv) {
               // by webpacks internal loaders.
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
-                name: 'static/media/[name].[hash:8].[ext]',
+                name: `${productName}/static/media/[name].[hash:8].[ext]`,
               },
             },
             // ** STOP ** Are you adding a new loader?
@@ -641,8 +641,8 @@ module.exports = function(webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: `${productName}/static/css/[name].[contenthash:8].css`,
+          chunkFilename: `${productName}/static/css/[name].[contenthash:8].chunk.css`,
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
